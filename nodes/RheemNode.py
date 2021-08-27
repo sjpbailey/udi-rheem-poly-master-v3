64b0e4d9af74c90e49bdc8b228d97b69f3174a94
sjpbailey <sjpbailey1961@gmail.com>
2021-08-27 14:10:36 -0700

    Commits

--- nodes/RheemNode.py
+++ nodes/RheemNode.py
@@ -53,19 +53,19 @@ class RheemNode(udi_interface.Node):
                 for equipment in equip_list:
                     LOGGER.info(f"\nName: {equipment.device_name}\n")
                     
-                    LOGGER.info(f"\nSerial #: {equipment.serial_number}\n")
-                    self.setDriver('GV3', str(f"{equipment.serial_number}"))
-                    
                     LOGGER.info(f"\nSet point: {equipment.set_point}\n")
                     self.setDriver('GV1', str(f"{equipment.set_point}"))
                     
                     LOGGER.info(f"\nOperation mode: {equipment.mode.value}\n")  # Operation mode: WaterHeaterOperationMode.GAS
                     self.setDriver('GV2', int(f"{equipment.mode.value}"))
+
+                    LOGGER.info(f"\nSerial #: {equipment.serial_number}\n")
+                    self.setDriver('GV3', str(f"{equipment.serial_number}"))
                     
                     LOGGER.info(f"\nOperation modes: {equipment.modes}\n")  # Operation modes: [<WaterHeaterOperationMode.OFF: 1>, <WaterHeaterOperationMode.GAS: 6>]
                     self.setDriver('GV4', str(f"{equipment.modes}"))
                     
-                    LOGGER.info("{}" .format(f"{equipment.set_point}"))
+                    #LOGGER.info("{}" .format(f"{equipment.set_point}"))
                     
                     LOGGER.info(f"\nOperation mode: {equipment.device_id}\n")
                     self.setDriver('GV5', str(f"{equipment.device_id}"))
