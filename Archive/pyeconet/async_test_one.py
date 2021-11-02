727533de95bcd8f1b41f2b696b7f4a4ef5a04df7
sjpbailey <sjpbailey1961@gmail.com>
2021-11-01 22:36:38 -0700

    set pyenv as environment

--- Archive/pyeconet/async_test_one.py
+++ Archive/pyeconet/async_test_one.py
@@ -1,31 +1,31 @@
 import asyncio
 import logging
-#import time
-#import getpass
+# import time
+# import getpass
 import requests
-from requests.auth import HTTPBasicAuth #HTTP
+from requests.auth import HTTPBasicAuth  # HTTP
 
 from pyeconet import EcoNetApiInterface
 from pyeconet.equipment import EquipmentType
 
-#logging.basicConfig()
-#logging.getLogger().setLevel(logging.DEBUG)
+
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
 
 async def main():
-    
-    email = "sjpbailey@comcast.net" #input("Enter your email: ").strip()
-    password = "NatiqueRheem61" #getpass.getpass(prompt='Enter your password: ')  
+    email = "sjpbailey@comcast.net"  # input("Enter your email: ").strip()
+    password = "NatiqueRheem61"  # getpass.getpass(prompt='Enter your password: ')
     api = await EcoNetApiInterface.login(email, password)
     all_equipment = await api.get_equipment_by_type([EquipmentType.WATER_HEATER])
-    
+
     try:
-        #r = requests.get(url, auth=HTTPBasicAuth(self.ipaddress, self.username, self.password))
+        # r = requests.get(url, auth=HTTPBasicAuth(self.ipaddress, self.username, self.password))
         api = await EcoNetApiInterface.login(email, password=password)
         r = all_equipment = await api.get_equipment_by_type([EquipmentType.WATER_HEATER])
-        
+
         for equip_list in all_equipment.values():
             for equipment in equip_list:
-                #print(str(equip_list))
+                # print(str(equip_list))
                 print(f"\nName: {equipment.device_name}\n")
                 print(f"\nSerial #: {equipment.serial_number}\n")
                 print(f"\nDevice ID: {equipment.device_id}\n")
@@ -33,25 +33,25 @@ async def main():
                 print(f"\nOperation mode: {equipment.mode}\n")
                 print(f"\nOperation modes: {equipment.modes}\n")
                 print(f"\nRunning ?: {equipment.running}\n")
-                #print(f"\nOperation modes: {equipment.phone_number}\n") #WaterHeaterOperationMode
+                # print(f"\nOperation modes: {equipment.phone_number}\n") #WaterHeaterOperationMode
                 print(type({equipment}))
-                #print(f"\nOperation modes: {equipment}\n") #set_point_limits
-                #print(f"\nOperation modes: {equipment}\n")
+                # print(f"\nOperation modes: {equipment}\n") #set_point_limits
+                # print(f"\nOperation modes: {equipment}\n")
                 equipment.set_set_point(equipment._api.publish('130', {equipment.device_id}, {equipment.serial_number}))
-                #api.publish(0, {equipment.device_id}, {equipment.serial_number})
+                # api.publish(0, {equipment.device_id}, {equipment.serial_number})
                 print(type({equipment.set_point}))
-                #await asyncio.sleep(300000)
-                print( str(equipment.modes[-1]))  #WaterHeaterOperationMode.GAS #{}".format(var2,var1))
-                print(str(equipment.modes[0])) #WaterHeaterOperationMode.OFF
-                print(str(equipment.modes[::])) #WaterHeaterOperationMode.OFF
-                print( str(equipment.device_id)) #    # "{}".format(var2,var1))
-                #api:["results"]["locations"][0]["equiptments"][0]['@SETPOINT']['value'] = '135'
-                
-                #equipment._api.publish({equipment.set_point}, {equipment.device_id}, {equipment.serial_number})
+                # await asyncio.sleep(300000)
+                print(str(equipment.modes[-1]))  # WaterHeaterOperationMode.GAS #{}".format(var2,var1))
+                print(str(equipment.modes[0]))  # WaterHeaterOperationMode.OFF
+                print(str(equipment.modes[::]))  # WaterHeaterOperationMode.OFF
+                print(str(equipment.device_id))  # # "{}".format(var2,var1))
+                # api:["results"]["locations"][0]["equiptments"][0]['@SETPOINT']['value'] = '135'
+
+                # equipment._api.publish({equipment.set_point}, {equipment.device_id}, {equipment.serial_number})
                 print(equipment.set_point)
 
             return equip_list
-            
+
         else:
             print.error("Rheem Econet Error:  " + equip_list)
             return None
@@ -59,6 +59,7 @@ async def main():
     except Exception as e:
         print("Error: " + str(e))
 
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())        
+    loop.run_until_complete(main())
