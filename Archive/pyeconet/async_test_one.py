a036803033a474d181e0a7461bd7ec9227fd4e4d
sjpbailey <sjpbailey1961@gmail.com>
2021-11-18 08:17:43 -0800

    Update async_test_one.py

--- Archive/pyeconet/async_test_one.py
+++ Archive/pyeconet/async_test_one.py
@@ -12,7 +12,8 @@ from pyeconet.equipment import EquipmentType
 
 async def main():
     email = "sjpbailey@comcast.net"  # input("Enter your email: ").strip()
-    password = "NatiqueRheem61"  # getpass.getpass(prompt='Enter your password: ')
+    # getpass.getpass(prompt='Enter your password: ')
+    password = "NatiqueRheem61"
     api = await EcoNetApiInterface.login(email, password)
     all_equipment = await api.get_equipment_by_type([EquipmentType.WATER_HEATER])
 
@@ -20,6 +21,7 @@ async def main():
         # r = requests.get(url, auth=HTTPBasicAuth(self.ipaddress, self.username, self.password))
         api = await EcoNetApiInterface.login(email, password=password)
         r = all_equipment = await api.get_equipment_by_type([EquipmentType.WATER_HEATER])
+        # w = all_equipment = await api.post_equipment_by_type([EquipmentType.WATER_HEATER])
 
         for equip_list in all_equipment.values():
             for equipment in equip_list:
@@ -30,7 +32,7 @@ async def main():
                 print(f"\nSet point: {equipment.set_point}\n")
                 print(f"\nOperation mode: {equipment.mode}\n")
                 print(f"\nOperation modes: {equipment.modes}\n")
-                print(f"\nRunning ?: {equipment.running}\n")
+                print(f"\nRunning ?: {equipment.running_state}\n")
                 # print(f"\nOperation modes: {equipment.phone_number}\n") #WaterHeaterOperationMode
                 # print(type({equipment}))
                 # print(f"\nOperation modes: {equipment}\n") #set_point_limits
@@ -42,9 +44,10 @@ async def main():
                 a = 137
                 a = str(a)
 
-                equipment.set_set_point(api.publish(str("@SETPOINT", "120"), {equipment.device_id}, {equipment.serial_number}))
+                equipment.set_set_point(5 + equipment.set_point)
                 # await asyncio.sleep(300000)
-                print(str(equipment.modes[-1]))  # WaterHeaterOperationMode.GAS #{}".format(var2,var1))
+                # WaterHeaterOperationMode.GAS #{}".format(var2,var1))
+                print(str(equipment.modes[-1]))
                 print(str(equipment.modes[0]))  # WaterHeaterOperationMode.OFF
                 print(str(equipment.modes[::]))  # WaterHeaterOperationMode.OFF
                 print(str(equipment.device_id))  # # "{}".format(var2,var1))
