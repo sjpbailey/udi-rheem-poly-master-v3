832cb969842225ca2847c0ccbe3e0339f98350e7
sjpbailey <sjpbailey1961@gmail.com>
2021-11-27 22:06:26 -0800

    Commit

--- Archive/pyeconet/equipment/water_heater.py
+++ Archive/pyeconet/equipment/water_heater.py
@@ -1,307 +1,183 @@
-"""EcoNet water heater"""
-
-from datetime import datetime
+"""Support for Rheem EcoNet water heaters."""
 import logging
-from enum import Enum
-from typing import List, Union
 
-from pyeconet.errors import InvalidResponseFormat
-from pyeconet.equipment import Equipment
+from pyeconet.equipment import EquipmentType
+from pyeconet.equipment.water_heater import WaterHeaterOperationMode
+
+from homeassistant.components.water_heater import (
+    ATTR_TEMPERATURE,
+    STATE_ECO,
+    STATE_ELECTRIC,
+    STATE_GAS,
+    STATE_HEAT_PUMP,
+    STATE_HIGH_DEMAND,
+    STATE_OFF,
+    STATE_PERFORMANCE,
+    SUPPORT_AWAY_MODE,
+    SUPPORT_OPERATION_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
+    WaterHeaterEntity,
+)
+from homeassistant.const import TEMP_FAHRENHEIT
+from homeassistant.core import callback
+
+from . import EcoNetEntity
+from .const import DOMAIN, EQUIPMENT
 
 _LOGGER = logging.getLogger(__name__)
 
+ATTR_ON_VACATION = "on_vacation"
+ATTR_IN_USE = "in_use"
 
-class WaterHeaterOperationMode(Enum):
-    """Define the operation mode"""
+ECONET_STATE_TO_HA = {
+    WaterHeaterOperationMode.ENERGY_SAVING: STATE_ECO,
+    WaterHeaterOperationMode.HIGH_DEMAND: STATE_HIGH_DEMAND,
+    WaterHeaterOperationMode.OFF: STATE_OFF,
+    WaterHeaterOperationMode.HEAT_PUMP_ONLY: STATE_HEAT_PUMP,
+    WaterHeaterOperationMode.ELECTRIC_MODE: STATE_ELECTRIC,
+    WaterHeaterOperationMode.GAS: STATE_GAS,
+    WaterHeaterOperationMode.PERFORMANCE: STATE_PERFORMANCE,
+}
+HA_STATE_TO_ECONET = {value: key for key, value in ECONET_STATE_TO_HA.items()}
 
-    OFF = 1
-    ELECTRIC_MODE = 2
-    ENERGY_SAVING = 3
-    HEAT_PUMP_ONLY = 4
-    HIGH_DEMAND = 5
-    GAS = 6
-    ENERGY_SAVER = 7
-    PERFORMANCE = 8
-    VACATION = 9
-    ELECTRIC = 10
-    HEAT_PUMP = 11
-    UNKNOWN = 99
-
-    @staticmethod
-    def by_string(str_value: str):
-        """Convert a string to a supported OperationMode"""
-        _cleaned_string = str_value.rstrip().replace(" ", "_").upper()
-        if _cleaned_string == WaterHeaterOperationMode.OFF.name.upper():
-            return WaterHeaterOperationMode.OFF
-        elif _cleaned_string == WaterHeaterOperationMode.ELECTRIC_MODE.name.upper():
-            return WaterHeaterOperationMode.ELECTRIC_MODE
-        elif _cleaned_string == WaterHeaterOperationMode.ENERGY_SAVING.name.upper():
-            return WaterHeaterOperationMode.ENERGY_SAVING
-        elif _cleaned_string == WaterHeaterOperationMode.HEAT_PUMP_ONLY.name.upper():
-            return WaterHeaterOperationMode.HEAT_PUMP_ONLY
-        elif _cleaned_string == WaterHeaterOperationMode.HIGH_DEMAND.name.upper():
-            return WaterHeaterOperationMode.HIGH_DEMAND
-        elif _cleaned_string == WaterHeaterOperationMode.GAS.name.upper():
-            return WaterHeaterOperationMode.GAS
-        elif _cleaned_string == WaterHeaterOperationMode.ENERGY_SAVER.name.upper():
-            # Treat ENERGY SAVER and ENERGY SAVING modes the same
-            return WaterHeaterOperationMode.ENERGY_SAVING
-        elif _cleaned_string == WaterHeaterOperationMode.PERFORMANCE.name.upper():
-            return WaterHeaterOperationMode.PERFORMANCE
-        elif _cleaned_string == WaterHeaterOperationMode.VACATION.name.upper():
-            return WaterHeaterOperationMode.VACATION
-        elif _cleaned_string == WaterHeaterOperationMode.ELECTRIC.name.upper():
-            # Treat ELECTRIC MODE and ELECTRIC modes the same
-            return WaterHeaterOperationMode.ELECTRIC_MODE
-        elif _cleaned_string == WaterHeaterOperationMode.HEAT_PUMP.name.upper():
-            # Treat HEAT PUMP ONLY and HEAT PUMP modes the same
-            return WaterHeaterOperationMode.HEAT_PUMP_ONLY
-        else:
-            _LOGGER.error("Unknown mode: [%s]", str_value)
-            return WaterHeaterOperationMode.UNKNOWN
+SUPPORT_FLAGS_HEATER = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
 
 
-class WaterHeater(Equipment):
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up EcoNet water heater based on a config entry."""
+    equipment = hass.data[DOMAIN][EQUIPMENT][entry.entry_id]
+    async_add_entities(
+        [
+            EcoNetWaterHeater(water_heater)
+            for water_heater in equipment[EquipmentType.WATER_HEATER]
+        ],
+    )
 
-    def __init__(self, equipment_info: dict, api_interface) -> None:
+
+class EcoNetWaterHeater(EcoNetEntity, WaterHeaterEntity):
+    """Define a Econet water heater."""
+
+    def __init__(self, water_heater):
         """Initialize."""
-        super().__init__(equipment_info, api_interface)
-        self.energy_usage = None
-        self._energy_type = None
-        self.water_usage = None
+        super().__init__(water_heater)
+        self._running = water_heater.running
+        self._poll = True
+        self.water_heater = water_heater
+        self.econet_state_to_ha = {}
+        self.ha_state_to_econet = {}
+
+    @callback
+    def on_update_received(self):
+        """Update was pushed from the ecoent API."""
+        if self._running != self.water_heater.running:
+            # Water heater running state has changed so check usage on next update
+            self._poll = True
+            self._running = self.water_heater.running
+        self.async_write_ha_state()
 
     @property
-    def leak_installed(self) -> bool:
-        """Return if heater has leak detection or not"""
-        leak = self._equipment_info.get("@LEAKINSTALLED")
-        if leak is not None:
-            return leak["value"] == 1
-        else:
-            return False
+    def is_away_mode_on(self):
+        """Return true if away mode is on."""
+        return self._econet.away
 
     @property
-    def has_shutoff_valve(self) -> bool:
-        return self._equipment_info.get("@VALVE") is not None
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return TEMP_FAHRENHEIT
 
     @property
-    def running(self) -> bool:
-        """Return if the water heater is running or not"""
-        return self._equipment_info.get("@RUNNING") != ""
-
-    @property
-    def running_state(self) -> str:
-        """Return the raw running value"""
-        return self._equipment_info.get("@RUNNING")
-
-    @property
-    def tank_hot_water_availability(self) -> Union[int, None]:
-        """Return the hot water availability"""
-        icon = self._equipment_info.get("@HOTWATER")
-        value = 100
-        if icon is None:
-            _LOGGER.debug("Tank does not support hot water capacity")
-            return None
-        if "ic_tank_hundread_percent" in icon:
-            value = 100
-        elif "ic_tank_fourty_percent" in icon:
-            value = 40
-        elif "ic_tank_ten_percent" in icon:
-            value = 10
-        elif "ic_tank_empty" in icon or "ic_tank_zero_percent" in icon:
-            # Tank is empty when shutoff valve is closed
-            value = 0
-        else:
-            _LOGGER.error("Invalid tank level: %s", icon)
-        return value
-
-    @property
-    def shutoff_valve_open(self) -> bool:
-        """Return if the shutoff valve is open or not"""
-        return self._equipment_info.get("@VALVE")["value"] == 0
-
-    @property
-    def tank_health(self) -> Union[int, None]:
-        """Return the value 0-100? of the tank/heating element health"""
-        return self._equipment_info.get("@TANK", {}).get("value")
-
-    @property
-    def compressor_health(self) -> Union[int, None]:
-        """Return the value 0-100 of the compressor for heat pump units health"""
-        return self._equipment_info.get("@COMBUSTION", {}).get("value")
-
-    @property
-    def demand_response_over(self) -> bool:
-        """Return if the demand response is running or not"""
-        return self._equipment_info.get("@VALVE")["value"] == 0
-
-    def _supports_modes(self) -> bool:
-        """Return if the system supports modes or not"""
-        return self._equipment_info.get("@MODE") is not None
-
-    def _supports_on_off(self) -> bool:
-        """Return if the system supports on and off"""
-        return self._equipment_info.get("@ENABLED") is not None
-
-    @property
-    def modes(self) -> List[WaterHeaterOperationMode]:
-        """Return a list of supported operation modes"""
-        _supported_modes = []
-        if self._supports_modes():
-            _modes = self._equipment_info.get("@MODE")["constraints"]["enumText"]
-            for _mode in _modes:
-                _op_mode = WaterHeaterOperationMode.by_string(_mode)
-                if _op_mode is not WaterHeaterOperationMode.UNKNOWN:
-                    _supported_modes.append(_op_mode)
-        if self._supports_on_off() and not _supported_modes:
-            _supported_modes.append(WaterHeaterOperationMode.OFF)
-            if self.generic_type == "gasWaterHeater" or self.generic_type == "tanklessWaterHeater":
-                _supported_modes.append(WaterHeaterOperationMode.GAS)
-            else:
-                _supported_modes.append(WaterHeaterOperationMode.ELECTRIC_MODE)
-        elif self._supports_on_off() and _supported_modes:
-            _supported_modes.append(WaterHeaterOperationMode.OFF)
-        return _supported_modes
-
-    @property
-    def mode(self) -> Union[WaterHeaterOperationMode, None]:
-        """Return the current mode"""
-        if self._supports_on_off():
-            if not self.enabled:
-                return WaterHeaterOperationMode.OFF
-        if self._supports_modes():
-            return self.modes[self._equipment_info.get("@MODE")["value"]]
-        else:
-            if self.generic_type == "gasWaterHeater" or self.generic_type == "tanklessWaterHeater":
-                return WaterHeaterOperationMode.GAS
-            else:
-                return WaterHeaterOperationMode.ELECTRIC_MODE
-
-    @property
-    def enabled(self) -> Union[bool, None]:
-        """Return the the water heater is enabled or not"""
-        if self._supports_modes():
-            return self.modes[self._equipment_info.get("@MODE")["value"]] != "OFF"
-        elif self._supports_on_off():
-            return self._equipment_info.get("@ENABLED")["value"] == 1
-        else:
-            # Unit doesn't support on/off or modes
-            return None
-
-    @property
-    def override_status(self) -> str:
-        """Return the alert override status"""
-        return self._equipment_info.get("@OVERRIDESTATUS")
-
-    @property
-    def todays_energy_usage(self) -> Union[float, None]:
-        return self.energy_usage
-
-    @property
-    def energy_type(self) -> str:
-        """Return the energy type returned from the energy usage response."""
-        return self._energy_type
-
-    @property
-    def todays_water_usage(self) -> Union[float, None]:
-        return self.water_usage
-
-    async def get_energy_usage(self):
-        """Call dynamic action for energy usage."""
-        date = datetime.now()
-        payload = {
-            "ACTION": "waterheaterUsageReportView",
-            "device_name": f"{self.device_id}",
-            "serial_number": f"{self.serial_number}",
-            "graph_data": {
-                "format": "daily",
-                "month": f"{date.month}",
-                "period": f"{date.day}",
-                "year": f"{date.year}"
-            },
-            "usage_type": "energyUsage"
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        _attr = {
+            ATTR_ON_VACATION: self._econet.vacation,
+            ATTR_IN_USE: self._econet.running,
         }
-        try:
-            _response = await self._api.get_dynamic_action(payload)
-        except InvalidResponseFormat:
-            _LOGGER.debug("Tried to get energy usage, but unit doesn't support it.")
-            return
-        _todays_usage = 0
-        for value in _response["results"]["energy_usage"]["data"]:
-            _todays_usage += value["value"]
-        self.energy_usage = _todays_usage
-        try:
-            self._energy_type = _response["results"]["energy_usage"]["message"].split(" ")[3].upper()
-        except (KeyError, IndexError):
-            _LOGGER.error("Failed to determine energy type from response.")
-            if self.generic_type == "gasWaterHeater":
-                self._energy_type == "KBTU"
-            else:
-                self._energy_type == "KWH"
-        _LOGGER.debug(_todays_usage)
+        return _attr
 
-    async def get_water_usage(self):
-        """Call dynamic action for water usage."""
-        date = datetime.now()
-        payload = {
-            "ACTION": "waterheaterUsageReportView",
-            "device_name": f"{self.device_id}",
-            "serial_number": f"{self.serial_number}",
-            "graph_data": {
-                "format": "daily",
-                "month": f"{date.month}",
-                "period": f"{date.day}",
-                "year": f"{date.year}"
-            },
-            "usage_type": "waterUsage"
-        }
-        try:
-            _response = await self._api.get_dynamic_action(payload)
-        except InvalidResponseFormat:
-            _LOGGER.debug("Tried to get water usage, but unit doesn't support it.")
-            return
-        _todays_usage = 0
-        for value in _response["results"]["water_usage"]["data"]:
-            _todays_usage += value["value"]
-        self.water_usage = _todays_usage
-        _LOGGER.debug(_todays_usage)
+    @property
+    def current_operation(self):
+        """Return current operation."""
+        econet_mode = self.water_heater.mode
+        _current_op = STATE_OFF
+        if econet_mode is not None:
+            _current_op = ECONET_STATE_TO_HA[econet_mode]
 
-    def set_mode(self, mode: WaterHeaterOperationMode):
-        """Set the provided mode or enable/disable if mode isn't support."""
-        payload = {}
-        if self._supports_modes() and self._supports_on_off():
-            if mode == WaterHeaterOperationMode.OFF:
-                payload["@ENABLED"] = 0
-            else:
-                if self._supports_on_off():
-                    payload["@ENABLED"] = 1
-                text_modes = self._equipment_info["@MODE"]["constraints"]["enumText"]
-                count = 0
-                for text_mode in text_modes:
-                    if mode == WaterHeaterOperationMode.by_string(text_mode):
-                        payload["@MODE"] = count
-                    count = count + 1
-        elif self._supports_modes():
-            text_modes = self._equipment_info["@MODE"]["constraints"]["enumText"]
-            count = 0
-            for text_mode in text_modes:
-                if mode == WaterHeaterOperationMode.by_string(text_mode):
-                    payload["@MODE"] = count
-                count = count + 1
-        elif self._supports_on_off():
-            if mode == WaterHeaterOperationMode.OFF:
-                payload["@ENABLED"] = 0
-            else:
-                payload["@ENABLED"] = 1
+        return _current_op
+
+    @property
+    def operation_list(self):
+        """List of available operation modes."""
+        econet_modes = self.water_heater.modes
+        op_list = []
+        for mode in econet_modes:
+            if (
+                mode is not WaterHeaterOperationMode.UNKNOWN
+                and mode is not WaterHeaterOperationMode.VACATION
+            ):
+                ha_mode = ECONET_STATE_TO_HA[mode]
+                op_list.append(ha_mode)
+        return op_list
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        if len(self.water_heater.modes) > 0:
+            if self.water_heater.supports_away:
+                return SUPPORT_FLAGS_HEATER | SUPPORT_AWAY_MODE
+            return SUPPORT_FLAGS_HEATER
+        if self.water_heater.supports_away:
+            return SUPPORT_TARGET_TEMPERATURE | SUPPORT_AWAY_MODE
+        return SUPPORT_TARGET_TEMPERATURE
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        target_temp = kwargs.get(ATTR_TEMPERATURE)
+        if target_temp is not None:
+            self.water_heater.set_set_point(target_temp)
         else:
-            _LOGGER.error("Unit doesn't support on off or modes, shouldn't being trying to set a mode.")
-        if payload:
-            self._api.publish(payload, self.device_id, self.serial_number)
+            _LOGGER.error("A target temperature must be provided")
 
-    def set_set_point(self, set_point: int):
-        """Set the equipment set point to set_point."""
-        lower, upper = self.set_point_limits
-        if lower <= set_point <= upper:
-            payload = {"@SETPOINT": set_point}
-            self._api.publish(payload, self.device_id, self.serial_number)
+    def set_operation_mode(self, operation_mode):
+        """Set operation mode."""
+        op_mode_to_set = HA_STATE_TO_ECONET.get(operation_mode)
+        if op_mode_to_set is not None:
+            self.water_heater.set_mode(op_mode_to_set)
         else:
-            _LOGGER.error("Set point out of range. Lower: %s Upper: %s Set point: %s", lower, upper, set_point)
+            _LOGGER.error("An operation mode must be provided")
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self.water_heater.set_point
+
+    @property
+    def min_temp(self):
+        """Return the minimum temperature."""
+        return self.water_heater.set_point_limits[0]
+
+    @property
+    def max_temp(self):
+        """Return the maximum temperature."""
+        return self.water_heater.set_point_limits[1]
+
+    @property
+    def should_poll(self) -> bool:
+        """Return True if entity has to be polled for state.
+
+        False if entity pushes its state to HA.
+        """
+        return self._poll
+
+    async def async_update(self):
+        """Get the latest energy usage."""
+        await self.water_heater.get_energy_usage()
+        await self.water_heater.get_water_usage()
+        self._poll = False
+
+    def turn_away_mode_on(self):
+        """Turn away mode on."""
+        self.water_heater.set_away_mode(True)
+
+    def turn_away_mode_off(self):
+        """Turn away mode off."""
+        self.water_heater.set_away_mode(False)
--- Archive/pyeconet/tester_node-poly.py
+++ Archive/pyeconet/tester_node-poly.py
@@ -10,68 +10,68 @@ from pyeconet.equipment import EquipmentType
 #from pyeconet.equipment.water_heater import WaterHeaterOperationMode
 from aiohttp import ClientSession, ClientTimeout
 from aiohttp.client_exceptions import ClientError
-#logging.basicConfig()
-#logging.getLogger().setLevel(logging.DEBUG)
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
 
 
 async def main():
-    email = "sjpbailey@comcast.net" #input("Enter your email: ").strip()
-    password = "NatiqueRheem61" #getpass.getpass(prompt='Enter your password: ')
+    email = "sjpbailey@comcast.net"  # input("Enter your email: ").strip()
+    # getpass.getpass(prompt='Enter your password: ')
+    password = "NatiqueRheem61"
     api = await EcoNetApiInterface.login(email, password=password)
-    all_equipment = await api.get_equipment_by_type([EquipmentType.WATER_HEATER]) #, EquipmentType.THERMOSTAT
-    #api.subscribe()
-    #await asyncio.sleep(5)
-    #for equip_list in all_equipment.values():
+    # , EquipmentType.THERMOSTAT
+    all_equipment = await api.get_equipment_by_type([EquipmentType.WATER_HEATER])
+    # api.subscribe()
+    # await asyncio.sleep(5)
+    # for equip_list in all_equipment.values():
     #    for equipment in equip_list:
-                #discover: {'address': 'controller', 'cmd': 'DISCOVER', 'query': {}}
-                #dict_values([[<pyeconet.equipment.water_heater.WaterHeater object at 0x10d4ed410>]])
+    #discover: {'address': 'controller', 'cmd': 'DISCOVER', 'query': {}}
+    # dict_values([[<pyeconet.equipment.water_heater.WaterHeater object at 0x10d4ed410>]])
     #            print(f"\nName: {all_equipment.values}")
     #            print("\n{equipment}")
     #            print("\n" f"{all_equipment.values}")
     #            print("\n{}" .format(equipment.device_name))
     #            print(f"\nName: {equipment.device_name}\n")
     #            print("\n")
-                #2print("{}" .format(equipment.set_point))
-                #3print("{}" .format(equipment.modes))
-                #4print("{}" .format(equipment.device_id))
-                #5print("{}" .format(equipment.serial_number))
-                #6print("{}" .format(equipment.mode))
-                #7print("{}" .format(all_equipment.values))
-                #print("{}" .format(logging.basicConfig()))
-                #print(logging.getLogger().setLevel(logging.DEBUG))
-                #payload = {"@SETPOINT": equipment.set_point}
-                #(payload, equipment.device_id, equipment.serial_number)
-                    #discover: {'address': 'controller', 'cmd': 'DISCOVER', 'query': {}}
-    #for options in all_equipment:
+    # 2print("{}" .format(equipment.set_point))
+    # 3print("{}" .format(equipment.modes))
+    # 4print("{}" .format(equipment.device_id))
+    # 5print("{}" .format(equipment.serial_number))
+    # 6print("{}" .format(equipment.mode))
+    # 7print("{}" .format(all_equipment.values))
+    #print("{}" .format(logging.basicConfig()))
+    # print(logging.getLogger().setLevel(logging.DEBUG))
+    #payload = {"@SETPOINT": equipment.set_point}
+    #(payload, equipment.device_id, equipment.serial_number)
+    #discover: {'address': 'controller', 'cmd': 'DISCOVER', 'query': {}}
+    # for options in all_equipment:
     #    for key in options:
     #        options[key] = int(options[key])
 
-    #for equip_list in all_equipment.values():
+    # for equip_list in all_equipment.values():
     #    print([equip_list])
     #    print("{}".format(all_equipment.values))
     #    print([all_equipment.values])
-        #print("\n{}" .format(equipment.device_name))
+    #print("\n{}" .format(equipment.device_name))
     for equip_list in all_equipment.values():
         for equipment in equip_list:
             #discover: {'address': 'controller', 'cmd': 'DISCOVER', 'query': {}}
-            #dict_values([[<pyeconet.equipment.water_heater.WaterHeater object at 0x10d4ed410>]])  
-            
+            # dict_values([[<pyeconet.equipment.water_heater.WaterHeater object at 0x10d4ed410>]])
+            equipment.set_set_point(125)
             print(f"\nName: {equipment.device_name}\n")
             print("{}" .format(equipment.set_point))
             print(f"\nOperation mode: {equipment.modes[0]}\n")
-            
-            
-            #payload = json.loads(equipment.set_set_point + 1) #payload = 140
-            #self._mqtt_client.publish(f"user/{self._account_id}/device/desired", payload=json.dumps(publish_payload))
-            #AttributeError: 'NoneType' object has no attribute 'publish'
-            #payload=json.dumps({"@SETPOINT": equipment.set_point+2})
-            #equipment.set_set_point(payload)
-            #equipment.set_set_point(payload)
-            #print(json.dump)
-            #print(all_equipment,{equipment.device_name}, {equipment.set_point==130})
-            #print(pyeconet.api)
-            #print(f"user/{equipment.user_id}/device/desired")
 
+            # payload = json.loads(equipment.set_set_point + 1) #payload = 140
+            #self._mqtt_client.publish(f"user/{self._account_id}/device/desired", payload=json.dumps(publish_payload))
+            # AttributeError: 'NoneType' object has no attribute 'publish'
+            #payload=json.dumps({"@SETPOINT": equipment.set_point+2})
+            # equipment.set_set_point(payload)
+            # equipment.set_set_point(payload)
+            # print(json.dump)
+            #print(all_equipment,{equipment.device_name}, {equipment.set_point==130})
+            # print(pyeconet.api)
+            # print(f"user/{equipment.user_id}/device/desired")
 
     """await asyncio.sleep(5)
     for equip_list in all_equipment.values():
@@ -95,7 +95,6 @@ async def main():
             #payload = {"@SETPOINT": set_point}
     #await asyncio.sleep(300000)
     #api.unsubscribe()"""
-
 
 
 if __name__ == "__main__":
