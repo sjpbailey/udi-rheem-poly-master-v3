b26694484a07e9560e7ba05e0b11b8d3fd41653c
sjpbailey <sjpbailey1961@gmail.com>
2023-10-28 04:28:49 -0700

    Update RheemNode.py

--- nodes/RheemNode.py
+++ nodes/RheemNode.py
@@ -69,7 +69,7 @@ class RheemNode(udi_interface.Node):
                     
                     LOGGER.info(f"\nSet point: {equipment.set_point}\n")
                     self.setDriver('GV1', str(f"{equipment.set_point}"))
-                    LOGGER.info(f"\nDriver GV7: 'GV7'\n")
+                    LOGGER.info(f"\nDriver GV7:" 'GV7')
                     equipment.set_point('GV7')
                     
                     LOGGER.info(f"\nOperation mode: {equipment.mode.value}\n")  # Operation mode: WaterHeaterOperationMode.GAS
